8e45ea222d6cef351f79f7286947627830d4a74c
Matheus7k <06matheus.oliveira@gmail.com>
2023-03-04 23:01:02 -0300

    Add project files.

---
+++ fatorial/Program.cs
@@ -0,0 +1,20 @@
+﻿int fatorial, fatorialTotal = 1;
+
+int Fatorial()
+{
+    if (fatorial == 0 || fatorial == 1)
+    {
+        return 1;
+    }
+    for (int i = 2; i <= fatorial; i++)
+    {
+        fatorialTotal *= i;
+    }
+
+    return fatorialTotal;
+}
+
+Console.Write("Digite um numero fatorial: ");
+fatorial = int.Parse(Console.ReadLine());
+
+Console.Write(Fatorial());
